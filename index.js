5ae6bb06d9f09408f687d7b30e767c111e717840
FerretCode <49322226+FerretCode@users.noreply.github.com>
2021-08-07 13:30:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
+let http = require('http');
 
-console.log('hi');
+class millie {
+  constructor(port, options) {
+    this.initialize = () => {
+      if(!parseInt(port))
+        return console.error(
+          'Port needs to be a valid number!'
+        );
+      
+      this.server = http.createServer();
+      
+      this.server.listen(port || 3000);
+      
+      console.log(
+        `Now listening on port ${port || 3000}`
+      );
+      
+      return this.server;
+    }
+    
+    this.request = (route, callback) => {
+      this.server.on('request', (req, res) => {
+        let url = route;
+        
+        console.log(url)
+        
+        if(!url.startsWith('/'))
+          url = '/' + url;
+          
+        if(req.url !== url)
+          return;
+          
+        callback(req, res);
+      });
+    }
+  }
+} 
+
+let millieInstance = new millie(3004, {});
+
+millieInstance.initialize();
+millieInstance.request('/', (req, res) => {
+  console.log(req);
+});
